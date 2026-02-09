eb474df0a1daf8678db09c1d8a0a754f1d0f4227
Trinath9395 <trinathdevops0606@gmail.com>
2026-02-09 13:53:43 +0530

    eks-aws-terraform

--- 30-rds/data.tf
+++ 30-rds/data.tf
@@ -10,3 +10,7 @@ data "aws_ssm_parameter" "database_subnet_group_name" {
   name = "/${var.project_name}/${var.environment}/database_subnet_group_name"
 }
 
+data "aws_ssm_parameter" "db_password" {
+  name = "/${var.project_name}/${var.environment}/db_password"
+  with_decryption = true
+}
